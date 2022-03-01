8818896fc0ba7cae6483a0968e87e3147ff1e510
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-01 23:15:47 +0530

    Input Fields

---
+++ Basics/input_fields.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from turtle import width
+
+from matplotlib.pyplot import text
+
+root = Tk()
+root.geometry("400x300")
+input_box = Entry(root)
+input_box.pack()
+
+def displayName():
+    name = Label(root, text=f"{input_box.get()}")
+    name.pack()
+    input_box.delete(0, END)
+
+btn = Button(root, text="Enter Name", command=displayName)
+btn.pack()
+
+root.mainloop()
