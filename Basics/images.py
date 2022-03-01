e38bb75cb7768754a6cdb03d0ca96a98039904ed
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-02 00:23:42 +0530

    Tkinter Images

---
+++ Basics/images.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Images")
+root.geometry("400x300")
+root.iconbitmap('Basics/icon.ico')
+
+img = ImageTk.PhotoImage(Image.open("Basics/Gojo.jfif"))
+lbl = Label(root, image=img)
+lbl.pack()
+
+exit_btn = Button(root, text="Exit Program", command=root.quit)
+exit_btn.pack()
+
+root.mainloop()
