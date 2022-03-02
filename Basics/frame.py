f9b4e06f49257647a9ebe4b154222d85cde34161
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-03 00:05:05 +0530

    Frames - Like Div

--- Basics/frame.py
+++ Basics/frame.py
@@ -6,8 +6,11 @@ root.title("Frames")
 root.geometry("400x300")
 root.iconbitmap('Basics/icon.ico')
 
-frame = LabelFrame(root, text="May be A Div Tag?", padx=5, pady=5)
+frame = LabelFrame(root, text="May be A Div Tag?", padx=35, pady=25, border=0)
 frame.pack(padx=10, pady=10)
+
+btn = Button(frame, text="Exit Program", command=root.quit)
+btn.pack()
 
 exit_btn = Button(root, text="Exit Program", command=root.quit)
 exit_btn.pack()
