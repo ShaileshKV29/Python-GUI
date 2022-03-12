bc34b5f6147263e6d2fbbf416e3d32040e565ba7
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-12 23:17:38 +0530

    New Window

---
+++ Basics/new_window.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title("New Window")
+
+top = Toplevel()
+top.title("TOP1")
+top2 = Toplevel()
+top2.title("TOP2")
+top3 = Toplevel()
+top3.title("TOP3")
+
+
+root.mainloop()
